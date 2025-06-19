2ebfb203ac633ae5c86d1c8953ce3252f5e1f150
maksym <paramudmaksym72@gmail.com>
2025-06-19 19:30:46 +0300

    Fix user rendering: each user has own container

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const input = document.querySelector("#search-user");
-const userContainer = document.querySelector(".user-container");
+const main = document.querySelector("main");
 
 async function renderList() {
   const result = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await result.json();
 
   for (const i of users) {
+    const userContainer = document.createElement("div");
+    userContainer.className = "user-container"; // стильове ім'я класу
+
     const userName = document.createElement("div");
     userName.className = "user-header";
     userName.innerHTML = i.name;
+
     userContainer.appendChild(userName);
+    main.appendChild(userContainer);
   }
 }
 
