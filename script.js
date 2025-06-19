a6cdaba00eaf52801453e2fe54b902dfdfb03882
maksym <paramudmaksym72@gmail.com>
2025-06-19 19:20:05 +0300

    Load users from API and display

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const input = document.querySelector("#search-user");
 const userContainer = document.querySelector(".user-container");
+
+async function renderList() {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await result.json();
+
+  for (const i of users) {
+    const userName = document.createElement("div");
+    userName.className = "user-header";
+    userName.innerHTML = i.name;
+    userContainer.appendChild(userName);
+  }
+}
+
+renderList();
